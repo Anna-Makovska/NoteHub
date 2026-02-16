dc6430446a15bc19899b00a9f4b653485303675e
Anna-Makovska <brain.lady404@gmail.com>
2026-02-16 19:30:50 +0200

    improved controller

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,25 +1,26 @@
 import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
+
 export const getAllNotes = async (req, res) => {
-  const { search, tag, page = 1, perPage = 10 } = req.query;
+  const { search, tag } = req.query;
+  const page = Number(req.query.page) || 1;
+  const perPage = Number(req.query.perPage) || 10;
 
-  const noteQuery = Note.find({
-    $or: [{ userId: req.user._id }, { isPublic: true }],
-  });
-
-  const skip = (page - 1) * perPage;
+  const filter = { userId: req.user._id };
 
   if (search) {
-    noteQuery.where({ $text: { $search: search } });
+    filter.$text = { $search: search };
   }
 
   if (tag) {
-    noteQuery.where('tag').equals(tag);
+    filter.tag = tag;
   }
 
+  const skip = (page - 1) * perPage;
+
   const [notes, totalNotes] = await Promise.all([
-    noteQuery.clone().skip(skip).limit(perPage),
-    noteQuery.countDocuments(),
+    Note.find(filter).skip(skip).limit(perPage),
+    Note.countDocuments(filter),
   ]);
 
   const totalPages = Math.ceil(totalNotes / perPage);
@@ -27,15 +28,22 @@ export const getAllNotes = async (req, res) => {
   res.status(200).json({ notes, totalNotes, totalPages, page, perPage });
 };
 
+
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findOne({ _id: noteId, userId: req.user._id });
+
+  const note = await Note.findOne({
+    _id: noteId,
+    $or: [{ userId: req.user._id }, { isPublic: true }],
+  });
+
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
 };
+
 
 export const createNote = async (req, res) => {
   const note = await Note.create({ ...req.body, userId: req.user._id });
